85e9c283f6502b1bf7c9b4024ea85e7e81d2cc42
Klinshy <79143155+Klinshy@users.noreply.github.com>
2024-05-22 11:09:49 +0200

    initial push

---
+++ Modern Ship Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Modern Ship Tileset" tilewidth="32" tileheight="32" tilecount="6400" columns="80">
+ <image source="Modern Ship Tileset.png" width="2560" height="2560"/>
+</tileset>
