77e306029095c6cb85bfacec2ae07bfd716bd198
Valdo <vteixeiraromao@gmail.com>
2024-06-05 11:50:45 +0100

    Cleaned tilesets (tsx, tsj, paths) + add WA repo structure

--- God ray linear 45 wide.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="God ray linear 45 wide" tilewidth="32" tileheight="32" tilecount="7750" columns="125">
- <image source="God ray linear 45 wide.png" width="4000" height="2000"/>
-</tileset>
